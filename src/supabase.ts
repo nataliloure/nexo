ffdc9a8c24c7925243dcdd87c09ebed96d14a65f
Natali L Nascimento <138037869+nataliloure@users.noreply.github.com>
2026-08-14 23:12:43 -0300

    Configure Supabase client

---
+++ src/supabase.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient(
+  'https://tpgzgwgasugacsmdhpkd.supabase.co',
+  'sb_publishable_IGqEXbp6zpA8SHhsnKhh5A_HcBD2e-R',
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+    },
+  },
+)
